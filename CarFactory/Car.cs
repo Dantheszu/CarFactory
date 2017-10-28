de98120ca40c8879231d6ede44f92eac6369ec88
dantheszu <dantheszu@gmail.com>
2017-10-28 14:37:59 +0200

    dodane auto

---
+++ CarFactory/Car.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarFactory
+{
+    class Car
+    {
+        public Car()
+        {
+            Marka = "markaPodstawowa";
+            Model = "modelPodstawowy";
+        }
+        public string Marka { get; private set; }
+
+        public string Model { get; private set; }
+
+        public void Drive(float dystans)
+        {
+            string jazda = "";
+
+            if (dystans > 0)
+            {
+                jazda = ("jade");
+            }
+            else if (dystans <= 0)
+            {
+                jazda = ("Stop");
+            }
+                      
+        }
+        public void Trąb()
+        {
+            
+        }
+
+    }
+
+}
